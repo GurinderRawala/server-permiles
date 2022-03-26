a1e10d04187cc3c6ec664b6040b1980f8345fc4d
Gurinder Singh <92389887+GurinderRawala@users.noreply.github.com>
2022-03-26 00:19:16 -0400

    added password hash in create-user-account

--- api/routes/user-accounts.js
+++ api/routes/user-accounts.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { hashPassword } = require('../../lib/encrypt');
 const { createAddUserAccount, createEditUserAccount } = require('../../user-account');
 const router = express.Router()
 
 exports.registerRoutes = (server, modules) =>{
-    router.post('/user-accounts/create-user-account', (req, res, next) => {
+    router.post('/user-accounts/create-user-account', async (req, res, next) => {
         const addUserAccount = createAddUserAccount(modules)
+        req.body.password = await hashPassword(req.body.password)
         addUserAccount(req.body, (err) => {
             if (err) { return next(err) }
               res.sendStatus(201)
