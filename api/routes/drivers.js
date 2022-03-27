ec9ecd15c4bf8cc4a904c497a43ce2a0fe9fe766
Mickey Dhami <mickey.dhami@yahoo.com>
2022-03-26 23:24:06 -0400

    add authentication to add driver route

--- api/routes/drivers.js
+++ api/routes/drivers.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const { createAddDriver, createUpdateDriver } = require('../../driver')
 
 exports.registerRoutes = (server, modules) => {
-    router.post('/drivers/add-driver', (req, res, next) => {
+  const { authenticationMiddlware : { determineUserRole, permissions } } = modules
+  const permissionAddDriver = permissions('driver:add')
+    router.post('/drivers/add-driver', [determineUserRole, permissionAddDriver] , (req, res, next) => {
       const addDriver = createAddDriver(modules)
       //Todo: translate req.body to Driver object
       addDriver(req.body, (err) => {
@@ -18,7 +20,7 @@ exports.registerRoutes = (server, modules) => {
         //Todo: translate req.body to Driver object
         updateDriver(req.body, (err) => {
           if (err) { return next(err) }
-            res.sendStatus(201)
+          res.sendStatus(201)
           next()
         })
     })
