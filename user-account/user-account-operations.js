90a44b470ffd3a07b249508d626ed44649318212
Mickey Dhami <mickey.dhami@yahoo.com>
2022-03-26 23:22:46 -0400

    add new user role to user account operations

--- user-account/user-account-operations.js
+++ user-account/user-account-operations.js
@@ -1,12 +1,3 @@
-module.exports.createAddUserAccount =  ({ userAccountRepo, log}) => {
-    const createAddUserAccount = addUserAccount.bind(null, userAccountRepo, log)
-    return createAddUserAccount
-  }
-module.exports.createEditUserAccount = ({ userAccountRepo, log  }) =>{
-   const createEditUserAccount =  editUserAccount.bind(null, userAccountRepo, log)
-   return createEditUserAccount
-}
-
 async function editUserAccount ( userAccountRepo, log, editQuery, callback) {
     log.info({editQuery}, 'updating driver')
     const res = await userAccountRepo.update(editQuery, {
@@ -23,4 +14,16 @@ async function addUserAccount ( userAccountRepo, log, userAccount, callback) {
     const res = await userAccountRepo.create(userAccount)
     log.info({res}, 'user account added')
     callback(null, res)
+}
+
+async function getUserRoleId (userAccountRepo, log, userId, callback) {
+  const res = await userAccountRepo.findByPk(userId)
+  callback(null, res.role)
+}
+  
+module.exports = {
+    createAddUserAccount : ({ userAccountRepo, log  }) => addUserAccount.bind(null, userAccountRepo, log),
+    createEditUserAccount : ({ userAccountRepo, log  }) => editUserAccount.bind(null, userAccountRepo, log),
+    createGetUserRoleById : ({ userAccountRepo, log  }) => getUserRoleId.bind(null, userAccountRepo, log )
   }
+  
