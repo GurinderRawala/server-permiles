2e1bd7f714759832a1dac4059decba058daaac5a
Mickey Dhami <mickey.dhami@yahoo.com>
2022-03-26 23:23:42 -0400

    add authentication middleware

---
+++ api/middleware/permissions.js
@@ -0,0 +1,19 @@
+const permissions = ({ log, rbac, enabled }) => (permission) => {  
+        return async (req, res, next) => {
+          if(!enabled) {
+            next()
+            return  
+          } 
+          
+          const allowed = await rbac.can('xx', permission)
+          if(!allowed) {
+            const errorMessage = `You don't have the permission to ${permission}. If you think this is in error, please contact support.`
+            const userId = req.session?.user?.id
+            log.info({ userId, err: errorMessage })
+            return res.status(403).json({ message: errorMessage })
+          }
+       next()
+    }
+}
+
+module.exports = permissions
