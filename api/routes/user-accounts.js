882cb6504d482360275af8fd41b3ba8a9d7d59af
Gurinder Singh <92389887+GurinderRawala@users.noreply.github.com>
2022-03-29 14:56:52 -0400

    updated permission names and permissions.js file

--- api/routes/user-accounts.js
+++ api/routes/user-accounts.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 exports.registerRoutes = (server, modules) =>{
     const { authenticationMiddlware : { determineUserRole, permissions } } = modules
-    const permissionCreateUserAccount = permissions('user:create')
+    const permissionCreateUserAccount = permissions('user-account:create')
     router.post('/user-accounts/create-user-account',[determineUserRole, permissionCreateUserAccount], (req, res, next) => {
         const addUserAccount = createAddUserAccount(modules)
         addUserAccount(req.body, (err) => {
@@ -13,7 +13,7 @@ exports.registerRoutes = (server, modules) =>{
             next()
         })
     })
-    const permissionEditUserAccount = permissions('user:edit')
+    const permissionEditUserAccount = permissions('user-account:edit')
     router.post('/user-accounts/edit-user-account',[determineUserRole, permissionEditUserAccount], (req, res, next) =>{
         const editUserAccount = createEditUserAccount(modules)
         editUserAccount(req.body, (err) =>{
