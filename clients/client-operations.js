0ad3aea510b104501734b7bd39bc70492fdaf005
Gurinder Singh <92389887+GurinderRawala@users.noreply.github.com>
2022-03-31 14:26:37 -0400

    updated client-opertions with suggested names

---
+++ clients/client-operations.js
@@ -0,0 +1,30 @@
+async function updateClient ( clientRepo, log, Client, callback) {
+    log.info({Client}, 'updating client')
+    const res = await clientRepo.update(Client, {
+        where: {
+            id: Client.id
+        }
+    });
+    log.info({res}, 'client updated')
+    callback(null, res)
+}
+
+async function addClientAccount ( clientRepo, log, Client, callback) {
+    log.info({Client}, 'adding client account')
+    const res = await clientRepo.create(Client)
+    log.info({res}, 'client account added')
+    callback(null, res)
+}
+
+async function getClientById (clientRepo, log, Client, callback) {
+    log.info({Client}, 'retrieving client information')
+    const res = await clientRepo.findByPk(Client.id)
+    log.info({Client}, 'client information retrieved')
+    callback(null, res)
+}
+  
+module.exports = {
+    createAddClient : ({ clientRepo, log  }) => addClientAccount.bind(null, clientRepo, log),
+    createUpdateClient : ({ clientRepo, log  }) => updateClient.bind(null, clientRepo, log),
+    createGetClientById : ({ clientRepo, log  }) => getClientById.bind(null, clientRepo, log )
+}
